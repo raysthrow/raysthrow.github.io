6885692ace75d4b116b53725739433b87c2d6af1
Ray Ensell <rayensell@gmail.com>
2019-08-08 23:35:00 -0400

    update

--- test.js
+++ test.js
@@ -1,17 +1,23 @@
-eqfeed_callback(
-{
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [102.0, 0.5]
-      },
-      "properties": {
-        "prop0": "value0"
-      }
-    },
-	{"type":"Feature","geometry":{"type":"Point","coordinates":[32.642926,-97.092968]},"properties":{"name":"Customer"}}
-  ]
+eqfeed_callback({
+	"type": "FeatureCollection",
+	"features": [{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [102.0, 0.5]
+			},
+			"properties": {
+				"prop0": "value0"
+			}
+		}, {
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [32.642926, -97.092968]
+			},
+			"properties": {
+				"prop1": "value1"
+			}
+		}
+	]
 })
