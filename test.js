b012c93175d06913bc1588bf1161a979dc936d99
Ray Ensell <rayensell@gmail.com>
2019-08-08 23:32:36 -0400

    update

--- test.js
+++ test.js
@@ -6,11 +6,12 @@ eqfeed_callback(
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [32.642926, -97.092968]
+        "coordinates": [102.0, 0.5]
       },
       "properties": {
         "prop0": "value0"
       }
-    }
+    },
+	{"type":"Feature","geometry":{"type":"Point","coordinates":[32.642926,-97.092968]},"properties":{"name":"Customer"}}
   ]
 })
